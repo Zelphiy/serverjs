310ed1c7b9b12673dd98a47792e7b2f2152b52f4
Axel <axelcastelot@gmail.com>
2024-02-07 12:59:57 +0100

    feat patch de server en javascript

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use((request, response, next) =>{
 
 client.connect(err =>{
     if(err){
-        crossOriginIsolated.log('Erreur à la connexion à la base de donnée')
+        console.log('Erreur à la connexion à la base de donnée')
     } else {
         console.log('Connexion réussie')
     }
@@ -47,7 +47,74 @@ app.post('/utilisateur', (request, response) => {
 
 });
 
-app.delete('/utilisateur/:id', (request, response) =>{
+app.delete('/utilisateur', (request, response) =>{
+    const {nom, prenom} = request.body;
+
+    if(!nom || !prenom){
+        return response.status(400).json({erreur : `Veuillez fournir un nom et un prénom!`});
+    };
+
+    const supprUtilisateur = {nom, prenom};
+    const collection = client.db("myDb").collection("Utilisateur");
+
+    try{
+        const result = collection.deleteOne(supprUtilisateur);
+        console.log(`Utilisateur supprimé avec succès`);
+        response.status(201).json(supprUtilisateur)
+    }
+    catch{
+        console.error(`Erreur lors de la suppression`, error)
+        response.status(500).json({erreur : `Erreur lors de la suppression d'utilisateur`})
+    }
+});
+
+// app.delete('/utilisateur/:name',(request,response)=>{
+//     const supprUtilisateur = request.params.name;
+//     if(!supprUtilisateur){
+//         res.status(400).send("Veuillez fournir un nom!");
+//     }else{
+//         const collection = client.db("myDb").collection("Utilisateur");
+//         collection.deleteOne({nom : supprUtilisateur}, (err,resultat)=>{
+//             if(err){
+//                 response.status(500).send("erreur serveur");
+//             }else{
+//                 response.status(200).send("utilisateur supprimer ");
+//             };
+//         });
+//     };
+// });
+
+app.get(`/utilisateur/:name`, (request, response)=>{
+    const getUtilisateur = request.params.name;
+    if(!getUtilisateur){
+        res.status(400).send("Veuillez fournir un nom!");
+    }else{
+        const collection = client.db("myDb").collection("Utilisateur");
+        collection.find({nom : getUtilisateur}).toArray((err, resultat)=>{
+            if(err){
+                response.status(500).send(`Erreur serveur`);
+            }else{
+                response.status(200).json(resultat);
+            };
+        });
+    };
+});
+
+app.patch(`/utilisateur/:name`, (request, response)=>{
+    const name = request.params.name;
+    const {nom, prenom} = request.body;
+    const collection = client.db("myDb").collection("Utilisateur");
+    const updateUtilisateur = {nom, prenom};
+
+    collection.update({nom: name}, updateUtilisateur, (err, resultat) =>{
+        if (err){
+            console.log(`Erreur lors de la mise à jour de l'utilisateur`);
+            response.status(500).send("Erreur interne du serveur")
+        }else{
+            response.json(resultat);
+            console.log(`Utilisateur mis à jour`);
+        }
+    });
 
 });
 
